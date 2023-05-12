8ec62bad264779056af8176ed60c69606dcd7d6e
Mesut Songur <mesut_songur_77@hotmail.com>
2023-05-12 16:14:59 +0300

    -

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import { Inter } from 'next/font/google'
+import styles from '@/styles/Home.module.css'
+
+const inter = Inter({ subsets: ['latin'] })
+import React from 'react'
+
+function App() {
+  return (
+    <div></div>
+  )
+}
+
+export default App
